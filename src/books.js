6bb69a8283924ebc242d2f7eededc4327bde1d5f
khandakarfahimahmed <khandakar.fahim.ahmed@g.bracu.ac.bd>
2023-06-06 22:16:04 +0600

    intial commit

---
+++ src/books.js
@@ -0,0 +1,45 @@
+import img1 from "./images/book-3.jpg";
+import img2 from "./images/book-1.jpg";
+import img3 from "./images/book-2.jpg";
+import img4 from "./images/book-4.jpg";
+import img5 from "./images/book-5.jpg";
+import img6 from "./images/book-6.jpg";
+
+export const books = [
+  {
+    author: "Bonnie Garmus",
+    title: "Lessons in Chemistry",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Abraham Verghese",
+    title: "The Covenant of Water",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Ramin Zahed",
+    title: "Spider-Man: Across the Spider-Verse: The Art of the Movie",
+    img: img3,
+    id: 3,
+  },
+  {
+    author: "Nicole Trope",
+    title: "The Family Across the Street",
+    img: img4,
+    id: 4,
+  },
+  {
+    author: "Charlie Donlea",
+    title: "Twenty Years Later",
+    img: img5,
+    id: 5,
+  },
+  {
+    author: "Anthony Doerr",
+    title: "All the Light We Cannot See",
+    img: img6,
+    id: 6,
+  },
+];
